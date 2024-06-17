47511b590b964d5c6f8815234cc516a62a941f0b
James Elazegui <jameselazegui21@gmail.com>
2024-06-17 20:00:01 +1200

    Load Passenger and Admin file
    Load files and store values in vectors.

--- flightless/Functions.cpp
+++ flightless/Functions.cpp
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <ctime>
 #include <fstream>
+
 using namespace std;
 
 unsigned int getDay()
@@ -72,6 +73,15 @@ void showPassengeMembers(const Passenger& target_)
 	cout << "Card expiry Year: " << target_.cardExpiryY << endl;
 }
 
+// cout struct members
+void showAdminMembers(const Admin& target_)
+{
+	cout << "First name: " << target_.firstName << endl;
+	cout << "Last name: " << target_.lastName << endl;
+	cout << "Username: " << target_.username << endl;
+	cout << "Password: " << target_.password << endl;
+}
+
 // check if input data type matches variable dataw type
 void inputValidation(int* target_)
 {
@@ -90,6 +100,14 @@ bool isNumber(const std::string& s)
 	return !s.empty() && std::find_if(s.begin(),
 		s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
 }
+
+
+
+/*
+		USER REGISTRATION
+*/
+
+
 
 // register new passenger
 void registerNewPassenger()
@@ -215,63 +233,4 @@ void registerNewAdmin()
 	cout << "New admin registration successful." << endl;
 }
 
-// test fstream file handling
-void fstreamTest()
-{
-	vector<Person> users;
-	// create an fstream object
-	fstream myFile;
 
-	// attempt to open file and set mode to in
-	myFile.open("users.txt", ios::in);
-
-	// check if file opened successfully
-	if (myFile.is_open())
-	{
-		// loop every line in file
-		string line;
-
-		// create temp person 
-		Person tempPerson;
-		while (getline(myFile, line))
-		{
-			// for each line check key if matches conditions
-			string key = line.substr(0, line.find(":"));
-			string val = line.substr(line.find(":") + 2);
-
-
-			// assign struct member value accordingly
-
-			if (key == "name")
-			{
-				tempPerson.name = val;
-			}
-			if (key == "age")
-			{
-				tempPerson.age = stoi(val);
-			}
-			if (key == "gender")
-			{
-				tempPerson.gender = stoi(val);
-			}
-			if (key == "mobile_number")
-			{
-				tempPerson.mobileNumber = val;
-			}
-			if (key == "country")
-			{
-				tempPerson.country = val;
-			}
-		}
-
-		cout << tempPerson.name << endl;
-		cout << tempPerson.age << endl;
-		cout << tempPerson.gender << endl;
-		cout << tempPerson.mobileNumber << endl;
-		cout << tempPerson.country << endl;
-	}
-	else
-	{
-		cout << "File not found!" << endl;
-	}
-}
--- flightless/flightless.cpp
+++ flightless/flightless.cpp
@@ -8,14 +8,127 @@
 #include <stdio.h>
 #include <ctime>
 #include <fstream>
+
 using namespace std;
+
+// function declaration
+void loadPassengerFile(vector<Passenger>* passengerVector_);
+//void loadDriverFile(vector<Driver>* driverVector);
+void loadAdminFile(vector<Admin>* adminVector);
+//void loadPassengerFile(vector<Booking>* bookingVector);
 
 int main()
 {
-	//registerNewAdmin();
-	fstreamTest();
+	// data vectors 
+	vector<Passenger> passengerVector;
+	//vector<Driver> driverVector;
+	vector<Admin> adminVector;
+	//vector<Booking> bookingVector;
+
+	loadPassengerFile(&passengerVector);
+	//loadDriverFile(&driverVector);
+	loadAdminFile(&adminVector);
+	//loadBookingFile(&bookingVector);
+
 
 	return 0;
 }
+
+// load passenger file
+void loadPassengerFile(vector<Passenger>* passengerVector_)
+{
+	string fileName = "passengers.txt"; // file to open
+	fstream file; // create an fstream object
+	Passenger tempUser; // create temp struct to store passenger data
+
+	// attempt to open file with ios method of input
+	file.open(fileName, ios::in);
+
+
+	if (file.is_open()) // check if file successfully opened
+	{
+		string line; // create a storage for each line's value read from the file
+
+		while (getline(file, line))
+		{
+			if (line.empty() || file.peek() == EOF)
+			{
+				(*passengerVector_).push_back(tempUser);
+				continue;
+			}
+
+			string key = line.substr(0, line.find(":"));
+			string val = line.substr(line.find(":") + 2);
+
+			if (key == "firstName")	tempUser.firstName = val;
+			if (key == "lastName")	tempUser.lastName = val;
+			if (key == "gender")	tempUser.gender = stoi(val);
+			if (key == "mobileNumber")	tempUser.mobileNumber = val;
+			if (key == "email")	tempUser.email = val;
+			if (key == "username")	tempUser.username = val;
+			if (key == "password")	tempUser.password = val;
+			if (key == "paymentMethod")	tempUser.paymentMethod = stoi(val);
+			if (key == "cardNumber")	tempUser.cardNumber = val;
+			if (key == "cardExpiryM")	tempUser.cardExpiryM = stoi(val);
+			if (key == "cardExpiryY")	tempUser.cardExpiryY = stoi(val);
+		}
+	}
+	else // if file fail to open
+	{
+		cout << "ERROR! Unable to open file " << fileName << endl;
+	}
+
+
+	for (int i = 0; i < (*passengerVector_).size(); i++)
+	{
+		showPassengeMembers((*passengerVector_)[i]);
+	}
+}
+
+// load admin file
+void loadAdminFile(vector<Admin>* adminVector_)
+{
+	string fileName = "admins.txt"; // file to open
+	fstream file; // create an fstream object
+	Admin tempUser; // create temp struct to store passenger data
+
+	// attempt to open file with ios method of input
+	file.open(fileName, ios::in);
+
+
+	if (file.is_open()) // check if file successfully opened
+	{
+		string line; // create a storage for each line's value read from the file
+
+		while (getline(file, line))
+		{
+			if (line.empty() || file.peek() == EOF)
+			{
+				(*adminVector_).push_back(tempUser);
+				continue;
+			}
+
+			string key = line.substr(0, line.find(":"));
+			string val = line.substr(line.find(":") + 2);
+
+			if (key == "firstName")	tempUser.firstName = val;
+			if (key == "lastName")	tempUser.lastName = val;
+			if (key == "username")	tempUser.username = val;
+			if (key == "password")	tempUser.password = val;
+
+		}
+	}
+	else // if file fail to open
+	{
+		cout << "ERROR! Unable to open file " << fileName << endl;
+	}
+
+
+	for (int i = 0; i < (*adminVector_).size(); i++)
+	{
+		showAdminMembers((*adminVector_)[i]);
+	}
+}
+
 
 
