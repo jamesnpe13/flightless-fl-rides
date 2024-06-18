b478154dcb947b02b99fec7c05d258e86955bc20
James Elazegui <jameselazegui21@gmail.com>
2024-06-18 22:59:33 +1200

    Temporary menu
    -created a temporary menu

--- flightless/flightless.cpp
+++ flightless/flightless.cpp
@@ -17,11 +17,47 @@ void loadFiles()
 	loadPassengerFile(&passengerVector);
 }
 
+void menu(int* userSelect_)
+{
+	cout << "1. Register new admin." << endl;
+	cout << "2. Show all admins." << endl;
+	cout << "3. Register new passenger." << endl;
+	cout << "4. Show all passengers." << endl;
+	cout << "5. Register new driver." << endl;
+	cout << "6. Show all drivers." << endl;
+
+	cin >> *userSelect_;
+}
+
 int main()
 {
 	while (1)
 	{
 		loadFiles();
+		int userSelect;
+
+		menu(&userSelect);
+
+		switch (userSelect)
+		{
+		case 1:
+			registerNewAdmin();
+			break;
+		case 2:
+			showAll(&adminVector);
+			break;
+		case 3:
+			registerNewPassenger();
+			break;
+		case 4:
+			showAll(&passengerVector);
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+
+		}
 	}
 
 	//cout << getDay() << "-" << getMonth() << "-" << getYear() << endl;
